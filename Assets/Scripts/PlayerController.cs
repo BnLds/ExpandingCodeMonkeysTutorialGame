e418d5123fe9b1a1ab555f508822d458fe678a4a
BnLds <landes.benjamin@gmail.com>
2023-03-13 12:18:39 +0100

    Added: collision check for movement using Capsule cast move towards X or Z if lateral movement is blocked

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,8 +13,51 @@ public class PlayerController : MonoBehaviour
     {
         
         Vector2 inputVector2Normalized = gameInput.GetMovementVectorNormalized();
+
         Vector3 moveDirection = new Vector3(inputVector2Normalized.x, 0, inputVector2Normalized.y);
-        transform.position += moveDirection * movementSpeed * Time.deltaTime;
+
+        float moveDistance = movementSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight ,playerRadius, moveDirection, moveDistance);
+
+        if(!canMove)
+        {
+            //cannot move towards direction
+
+            //attempt only X movement
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight ,playerRadius, moveDirectionX, moveDistance);
+
+            if(canMove)
+            {
+                //Can only move on the X
+                moveDirection = moveDirectionX;
+            } 
+            else 
+            {
+                //Cannot move only on the X
+                //Attempt only Z movement
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight ,playerRadius, moveDirectionZ, moveDistance);
+
+                if(canMove)
+                {
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    //cannot move in any direction 
+                }
+
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDirection * movementSpeed * Time.deltaTime;
+        }
+
         isWalking = moveDirection != Vector3.zero;
  
         float rotateSpeed = 10f;
