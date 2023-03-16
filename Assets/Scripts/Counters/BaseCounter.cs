2af927fa1a4c8017299004cd8dd056ae41714428
BnLds <landes.benjamin@gmail.com>
2023-03-16 10:50:22 +0100

    Stove counter logic implemented (State Machine) // Created BurningRecipeSO

--- Assets/Scripts/Counters/BaseCounter.cs
+++ Assets/Scripts/Counters/BaseCounter.cs
@@ -14,7 +14,7 @@ public class BaseCounter : MonoBehaviour, IKitchenObjectParent
     }
     public  virtual void InteractAlternate(PlayerController player)
     {
-        Debug.LogError("BaseCounter.InteractAlternate();");
+        //Debug.LogError("BaseCounter.InteractAlternate();");
     }
     public Transform GetKitchenObjectFollowTransform()
     {
