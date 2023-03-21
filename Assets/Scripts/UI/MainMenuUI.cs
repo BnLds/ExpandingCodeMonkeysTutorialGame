6d3df68e45e6a75961c67b8561ab6fec965b6048
BnLds <landes.benjamin@gmail.com>
2023-03-21 11:23:48 +0100

    Loading system // Main menu

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+
+
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() => 
+        {
+            //Click
+            Loader.Load(Loader.Scene.GameScene);
+        });
+
+        quitButton.onClick.AddListener(() => 
+        {
+            //Quit
+            Application.Quit();
+
+        });
+    }
+
+}
